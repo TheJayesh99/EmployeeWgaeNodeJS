aff94a1ba0ba9b602ef1c36c0634dc26af89451c
TheJayesh99 <jayeshm.official@gmail.com>
2021-08-03 17:44:50 +0530

    Added : Maximum employee working days

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -2,7 +2,8 @@ const IS_PART_TIME = 1
 const IS_FULL_TIME = 2
 const PART_TIME_HOURS = 4
 const FULL_TIME_HOURS = 8
-const WAGE_PER_HOUR =20
+const WAGE_PER_HOUR = 20
+const NUM_OF_WORKING_DAYS = 20
 
 function getWorkingHours(empCheck) {
     switch (empCheck) {
@@ -15,7 +16,11 @@ function getWorkingHours(empCheck) {
     }
 }
 
-let empCheck = Math.floor(Math.random()*10) % 3
-let empHours = getWorkingHours(empCheck)
+let empHours = 0
+for (let days = 0 ; days < NUM_OF_WORKING_DAYS ; days++){
+    let empCheck = Math.floor(Math.random()*10) % 3
+    empHours += getWorkingHours(empCheck)
+}
+
 let empWage =empHours * WAGE_PER_HOUR
-console.log("Employee Wage "+empWage)
+console.log("Total hours "+empHours+" Employee Wage "+empWage)
